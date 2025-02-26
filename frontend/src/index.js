4d883d933a3c52e41c631f4d29f159dc6b29c4ac
Benjamin Gibert <contact@benjamingibert.com>
2025-02-26 09:16:07 +0100

    first commit

---
+++ frontend/src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.scss';
+
+import reportWebVitals from './reportWebVitals';
+import Home from './pages/Home/Home';
+
+// import Error404 from './pages/Error404/Error404';
+// Utilisation de HashRouter pour que les différents composants soient rendus dynamiquement en fonction de l'URL, sans avoir besoin de recharger la page entière à chaque fois.
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
+import { LibrairiesProvider } from './utils/LibrairiesContext';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode> 
+    <Router>
+      <LibrairiesProvider>
+        {/* <Header/> */}
+        <Routes>
+          <Route path="/" element={<Home/>} />
+        </Routes>
+        {/* <Footer/> */}
+      </LibrairiesProvider>
+    </Router>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ frontend/src/pages/Home/Home.js
@@ -0,0 +1,20 @@
+import './Home.scss'
+// import React, { useContext } from 'react'
+import { Link } from 'react-router-dom'
+
+import { LibrairiesContext } from '../../utils/LibrairiesContext'
+import React, { useContext, useState, useEffect } from 'react'
+
+function Home () {
+
+    
+    return (
+        <main>
+            <section className='home'>
+                <p>page d'accueil</p>
+            </section>
+        </main>
+    )
+}
+
+export default Home
